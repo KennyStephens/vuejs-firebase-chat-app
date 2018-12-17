cff2f260f0dd1e6c053b7c81f6602e495a4daef7
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2018-12-17 13:08:18 -0700

    Included code from Firebase

---
+++ vue-chat/src/firebase/init.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyAWs7zixnPWpLX6G3b9EMAXF5OtI7kEYh0",
+    authDomain: "vuejs-firebase-01-21041.firebaseapp.com",
+    databaseURL: "https://vuejs-firebase-01-21041.firebaseio.com",
+    projectId: "vuejs-firebase-01-21041",
+    storageBucket: "vuejs-firebase-01-21041.appspot.com",
+    messagingSenderId: "101143222994"
+  };
+const firebaseApp = firebase.initializeApp(config);
+firebaseApp.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebassApp.firestore();
