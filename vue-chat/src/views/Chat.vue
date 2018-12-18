d6a35d5ab212b7fd59b92013d093369f1eb9fa48
Kenny Stephens <34525009+KennyStephens@users.noreply.github.com>
2018-12-18 08:23:03 -0700

    Working on chat component

---
+++ vue-chat/src/views/Chat.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container chat">
+        <h2 class="text-primary text-center">Real-Time Chat</h2>
+        <h5 class="text-secondary text-center">Powered by Vue.js and Firebase</h5>
+        <div class="card">
+            <div class="card-body">
+                <p class="text-secondary nomessages" v-if="messages.length == 0">
+                    [No Messages Yet!]
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
